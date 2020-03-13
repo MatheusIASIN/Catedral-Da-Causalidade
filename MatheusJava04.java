7c4020ad42766af926f482529bb421f925c066b6
Matheus Iasin <55819881+MatheusIASIN@users.noreply.github.com>
2020-03-13 16:08:36 -0300

    Add files via upload
    meus primeiros códigos em java

---
+++ MatheusJava04.java
@@ -0,0 +1,31 @@
+package br.com.Matheus;
+
+public class MatheusJava04 {
+	 
+	public static void main(String [] args) {
+	
+		int Num = 100;
+		String Sal =" 111 ";
+		System.out.println(Num);
+		System.out.println(Sal);
+		
+		/* Alerta!
+		 *  toda variavel  possui:
+		 *  um tipo 
+		 *  um tamanho
+		 *  um valor
+		 */
+		
+		/* Alerta! 2
+		 * 1) todo armazenamento na memoria  [e temporario
+		 * 2) os  dados são removidos da memoria quando o computador é desligado
+		 * 3)  a memoria é um tipo de armazenamento volatil
+		 * 4) o valor antigo é perdido pra sempre
+		 * 5) o Java Desaloca Memória
+		 * 
+		 */
+		
+		
+	}
+
+}
---
+++ matheusJava10.java
@@ -0,0 +1,11 @@
+package br.com.Matheus;
+
+public class matheusJava10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
