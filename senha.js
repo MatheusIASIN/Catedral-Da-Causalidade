68a9d06ec0c3f969f618ce62e98933666228cb50
Matheus Iasin <55819881+MatheusIASIN@users.noreply.github.com>
2020-06-16 13:46:30 -0300

    Add files via upload

---
+++ senha.js
@@ -0,0 +1,11 @@
+function validarsenhaforca(){
+var senha = document.getElementById("senhaforca").nodeValue;
+document.getElementById("impsenha").innerHTML="senha" +senha;
+
+if((senha.length >=4)&&(senha.length <7)){
+forca+=10;
+}else if (senha.length >7){
+    forca+=
+}
+
+}
