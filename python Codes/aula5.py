231e07dd579bedf45a12ce97363baba47262cc0b
Matheus Iasin <55819881+MatheusIASIN@users.noreply.github.com>
2020-06-28 21:21:02 -0300

    Add files via upload

---
+++ python Codes/aula5.py
@@ -0,0 +1,5 @@
+n=int(input("digite um valor: "))
+a = n - 1
+s = n + 1
+
+print("Analisando o valor {}, \n seu antecessor é {}\n e o  sucessor é {}".format(n,a,s))
---
+++ python Codes/printcoment.py
@@ -0,0 +1,13 @@
+
+
+
+
+print(" \n se é uma instrução de comentario")
+print(" Quando se usar o jogo-da-velha '#'você comentara só uma linha")
+print("quando precisar criar  blocos de comentarios se  determina no inicio as aspas duplas 3 vezes ")
+
+n=int(input("digite um valor: "))
+a = n - 1
+s = n + 1
+
+print("Analisando o valor {}, \n seu antecessor é {}\n e o  sucessor é {}".format(n,a
